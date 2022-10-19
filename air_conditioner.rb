68f978023aafb8b7affbb487a180360cd9ab9afc
EC2 Default User <amrumineq@gmail.com>
2022-10-19 05:47:01 +0000

    algorithm

---
+++ air_conditioner.rb
@@ -0,0 +1,47 @@
+input_lines = readlines
+
+room = input_lines[0].to_i
+air_conditioner = input_lines[1].to_i
+air_power = input_lines[2].to_i
+
+temperature_diff =(room - air_conditioner).abs
+
+require_time = 0
+
+if temperature_diff < 5
+    require_time = 15
+elsif temperature_diff >= 5 && temperature_diff < 10 then
+    require_time = 30
+elsif temperature_diff >= 10  then
+    require_time = 60
+end
+
+
+
+if air_power == 1
+   require_time
+elsif air_power== 2
+   require_time -= 5
+elsif air_power == 3
+   require_time -= 10
+else
+   p "風量の値は１～３にしてください"
+   exit
+end
+  
+p require_time
+
+=begin
+case air_power
+when 1
+  require_time
+when 2
+  require_time -= 5
+when 3
+  require_time -= 10
+else
+  p "風量の値は１～３にしてください"
+  exit
+end
+
+p require_time
